e95b4932946e43e3108a970a3da477cde8fa0ff4
Mia <121979781+MiaZhengLS@users.noreply.github.com>
2023-08-25 15:56:06 -0700

    Gamma correction

--- header/color.h
+++ header/color.h
@@ -3,6 +3,12 @@
 
 #include "vec3.h"
 using color = vec3;
+
+inline double linear_to_gamma(double linear_num)
+{
+  return sqrt(linear_num);
+}
+
 inline void write_color(std::ostream &out, color pixel_color, int sample_per_pixel)
 {
   double r = pixel_color.x();
@@ -13,6 +19,10 @@ inline void write_color(std::ostream &out, color pixel_color, int sample_per_pix
   r *= percent;
   g *= percent;
   b *= percent;
+
+  r = linear_to_gamma(r);
+  g = linear_to_gamma(g);
+  b = linear_to_gamma(b);
 
   static const interval intensity(0.000, 0.999);
   out << static_cast<int>(256 * intensity.clamp(r)) << ' '
