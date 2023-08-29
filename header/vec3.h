f8ae912f79ae213c0ec58918f55e2f4d3d0c7899
Mia <121979781+MiaZhengLS@users.noreply.github.com>
2023-08-28 18:38:49 -0700

    Included upper limit for random_int

--- header/vec3.h
+++ header/vec3.h
@@ -18,7 +18,7 @@ inline double random_double(double min, double max)
 
 inline int random_int(int min, int max)
 {
-  return static_cast<int>(random_double(min, max));
+  return static_cast<int>(random_double(min, max + 1));
 }
 
 class vec3
